bd64628f9c9dff13fa4ff4b681c7d64ce02653ea
Haris Sucipto <harisssucipto@gmail.com>
2017-12-31 17:04:22 +0700

    list resep

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const App = new Vue({
   el: '#app',
   data: {
+    lihatTambahResep: false,
+    lihatListResep: true,
+
     namaResep: '',
     descResep: '',
     kesulitanResep: '',
     lamaResep: '',
     bahanResep: [],
     langkahResep: [],
-    storeResep: []
+
+    storeResep: [
+      {
+        namaResep: 'ayam bekatul',
+        descResep: 'ayam yang sangat enak',
+        kesulitanResep: '1',
+        lamaResep: '2',
+        bahanResep: [
+          { nama: 'ayam', banyak: '2 kg' },
+          { nama: 'nasi', banyak: '1kg' },
+          { nama: 'ubi', banyak: '1kg' }
+        ],
+        langkahResep: [{ nama: 'kurangi bumbu' }, { nama: 'tambah bumbu' }]
+      }
+    ]
   },
   methods: {
     tambahBahan() {
@@ -25,6 +42,11 @@ const App = new Vue({
       this.langkahResep.pop();
     },
     tambahResep() {
+      if (!this.namaResep) {
+        alert('Nama Resep Harus dimasukan');
+        return;
+      }
+
       const {
         namaResep,
         descResep,
@@ -44,6 +66,15 @@ const App = new Vue({
       };
 
       this.storeResep.push(resepBaru);
+      this.hapusForm();
+    },
+    hapusForm() {
+      this.namaResep = '';
+      this.descResep = '';
+      this.kesulitanResep = '';
+      this.lamaResep = '';
+      this.bahanResep.splice(0);
+      this.langkahResep.splice(0);
     }
   }
 });
